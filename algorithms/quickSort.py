a88588d9695663d49c42f0e9fa6a8bc4011606bd
ruben <rubengarcia0515@gmail.com>
2025-05-11 11:14:47 -0500

    added review notes.

--- algorithms/quickSort.py
+++ algorithms/quickSort.py
@@ -1,5 +1,5 @@
 input1 = [3, 6, 8, 10, 1, 2, 1]
-
+## space complexity O(1), a "in-place" sorting algorithm
 
 def init(arr: list[int]):
     print("inputs:", arr)
@@ -15,15 +15,18 @@ def quickSort(start: int, end: int, arr: list[int]):
 
     pivot = arr[end]
     left = start
+    ##sets up pivot to end and keeps track of the left var to swap out
     for i in range(start,end):
         if arr[i] < pivot:
             tmp = arr[i]
             arr[i] = arr[left]
             arr[left] = tmp
             left += 1
+     ## swap of end results
     arr[end] = arr[left]
     arr[left] = pivot
     ##here we partition one less and greater than pivot since pivot is already in the correct place
     quickSort(start, left-1, arr)
     quickSort(left + 1, end, arr)
+    ## split in half recursively, this what enables to have a O(nlogn). NOTE: this algorithm can be a O(n^2) if the array was already sorted. Can navigate by swapping out last val with a random value generally
     return arr
